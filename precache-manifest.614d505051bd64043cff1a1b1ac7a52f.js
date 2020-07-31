fc5b44b72e200c54356544174c09e1288655acd9
Łukasz Wiącek <lukasz.wiacek@mayeryn.com>
2020-07-31 19:08:41 +0200

    Updates

---
+++ precache-manifest.614d505051bd64043cff1a1b1ac7a52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb8ce051a4fcdf3d48b2b5ebe1a3e27",
+    "url": "/Movie_Library/index.html"
+  },
+  {
+    "revision": "6e65dc5f6b080308f40d",
+    "url": "/Movie_Library/static/css/main.6113eca1.chunk.css"
+  },
+  {
+    "revision": "a0e72016e1a2349f12ae",
+    "url": "/Movie_Library/static/js/2.0ff4aeed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie_Library/static/js/2.0ff4aeed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e65dc5f6b080308f40d",
+    "url": "/Movie_Library/static/js/main.e2f7dd2b.chunk.js"
+  },
+  {
+    "revision": "a1b23e027c9f9fd2991e",
+    "url": "/Movie_Library/static/js/runtime-main.3b947427.js"
+  }
+]);
